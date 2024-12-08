a41e575e0b34179e2164758154bba595735963a7
alvaro aguirre <alguirre2@gmail.com>
2024-12-08 00:14:19 -0600

    4 css module

--- @5.2.3/src/components/Buttom/index.tsx
+++ @5.2.3/src/components/Buttom/index.tsx
@@ -1,19 +1,25 @@
 import { ReactNode } from "react";
-import "./Buttom.css";
+import styles from "./Buttom.module.css";
 
 type Props = {
   children: ReactNode;
   isLoading?: boolean;
   onClick: () => void;
 };
-
+console.log(styles);
 function Buttoms({ children, isLoading, onClick }: Props) {
+  const className = [
+    `btn btn-${isLoading ? "Secundary" : "primary"}`,
+    styles.button,
+  ].join(" ");
+
   return (
     <button
       disabled={isLoading}
       onClick={onClick}
       type="button"
-      className={`btn btn-${isLoading ? "Secundary" : "primary"}`}
+      //className={[styles.button, styles.padded].join(" ")}
+      className={className}
     >
       {isLoading ? "Cargando..." : children}
     </button>
